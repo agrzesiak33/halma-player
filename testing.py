90079ca25112836de675fa972ca0555efd15156f
Alex <ajg379@neu.edu>
2017-04-19 14:35:48 -0700

    1 color jumping, minor bug fixes

--- testing.py
+++ testing.py
@@ -65,11 +65,13 @@ class Board:
             legalMoves = self.generateLegalMoves(oldx, oldy)
             if [x,y] in legalMoves:
                 print("legal move")
-                self.handleGreenClick(event)
+                self.handlePieceClick(event)
             else:
                 print("illegal move")
                 self.listBoard[oldx * self.dimen + oldy][1] = "green"
+                self.listBoard[oldx * self.dimen + oldy][0].config(bg='white')
                 self.buttonJustClicked = None
+
 
 
 
@@ -80,33 +82,72 @@ class Board:
 
     def tester(self):
         self.listBoard[4][0].image = self.dark_green
-        self.listBoard[4][0].bind("<Button-1>", self.handleGreenClick)
+        self.listBoard[4][0].bind("<Button-1>", self.handlePieceClick)
         self.listBoard[4][0].config(image = self.dark_green)
         self.listBoard[4][1]="green"
 
-    def handleGreenClick(self, event):
-        print("Handling green click")
+        self.listBoard[5][0].image = self.dark_green
+        self.listBoard[5][0].bind("<Button-1>", self.handlePieceClick)
+        self.listBoard[5][0].config(image=self.dark_green)
+        self.listBoard[5][1] = "green"
+
+        self.listBoard[1][0].image = self.dark_red
+        self.listBoard[1][0].bind("<Button-1>", self.handlePieceClick)
+        self.listBoard[1][0].config(image=self.dark_red)
+        self.listBoard[1][1] = "red"
+
+        self.listBoard[2][0].image = self.dark_red
+        self.listBoard[2][0].bind("<Button-1>", self.handlePieceClick)
+        self.listBoard[2][0].config(image=self.dark_red)
+        self.listBoard[2][1] = "red"
+
+    def handlePieceClick(self, event):
+        print("Handling", self.turn, "click")
         xy = event.widget.text.split(",")
         x = int(xy[0])
         y = int(xy[1])
 
-        if self.turn is "green" and self.listBoard[x * self.dimen + y][1] is "green":
+        #   If it is greens turn and the space just clicked is green
+        if self.listBoard[x * self.dimen + y][1] is self.turn and self.buttonJustClicked is None:
             self.listBoard[x * self.dimen + y][1] = "selected"
+            self.listBoard[x * self.dimen + y][0].config(bg = 'blue')
             self.buttonJustClicked = self.listBoard[x * self.dimen + y][0]
-            print("Selected a green piece")
-        elif self.turn is "green" and self.buttonJustClicked is not None:
-            print("handling making teh selection")
+            print("Selected a ",self.turn," piece")
+        #   If there already was a selection and we need to move into the new spot
+        elif self.buttonJustClicked is not None:
             oldXY = self.buttonJustClicked.text.split(",")
-            self.listBoard[int(oldXY[0]) * self.dimen + int(oldXY[1])][1] = "empty"
-            self.buttonJustClicked.image = self.empty
-            self.buttonJustClicked.config(image = self.empty)
-            self.buttonJustClicked.unbind("<Button-1>")
+            oldX = int(oldXY[0])
+            oldY = int(oldXY[1])
+            if self.listBoard[x *self.dimen + y][1] is not "empty":
+                print("There is already a piece there")
+                #   Unselecting the button
+                self.listBoard[oldX * self.dimen + oldY][0].config(bg = 'white')
+                self.listBoard[oldX * self.dimen + oldY][1] = self.turn
+                self.buttonJustClicked = None
+            else:
+                print("handling making teh selection")
+                #   Getting rid of old tile
+                self.listBoard[oldX * self.dimen + oldY][1] = "empty"
+                self.listBoard[oldX * self.dimen + oldY][0].image = self.empty
+                self.listBoard[oldX * self.dimen + oldY][0].config(image = self.empty, bg = 'white')
+                self.listBoard[oldX * self.dimen + oldY][0].bind("<Button-1>", self.handleClick)
 
-            event.widget.image = self.dark_green
-            event.widget.config(image = self.dark_green)
-            self.listBoard[x*self.dimen + y][0].bind("<Button-1>", self.handleGreenClick)
-            self.listBoard[x * self.dimen + y][1] = "green"
-            self.buttonJustClicked = None
+                #   Changind the new square to the green tile
+                if self.turn is "green":
+                    self.listBoard[x * self.dimen + y][0].image = self.dark_green
+                    self.listBoard[x * self.dimen + y][0].config(image = self.dark_green)
+                else:
+                    self.listBoard[x * self.dimen + y][0].image = self.dark_red
+                    self.listBoard[x * self.dimen + y][0].config(image=self.dark_red)
+                self.listBoard[x * self.dimen + y][0].bind("<Button-1>", self.handlePieceClick)
+                self.listBoard[x * self.dimen + y][1] = self.turn
+                self.buttonJustClicked = None
+
+
+                if self.turn is "green":
+                    self.turn = "red"
+                else:
+                    self.turn = "green"
 
     def generateLegalMoves(self, x, y):
         legalMoves = []
@@ -116,8 +157,37 @@ class Board:
             if row >= 0 and row < self.dimen:
                 for column in range(y-1, y+2):
                     if column >= 0 and column < self.dimen:
+                        #   If sn adjacent space is empty it is obvious we can move there
                         if self.listBoard[row * self.dimen + column][1] == "empty":
                             legalMoves.append([row, column])
+
+                        #   If there is a piece there, we have to check for jumps
+                        else:
+                            self.findJumps([], legalMoves, x,y)
+
+        print(legalMoves)
         return legalMoves
 
+    def findJumps(self, visited, legalMoves, x, y):
+        visited.append([x,y])
+
+
+        for rowOffset in range(-1,2):
+            for columnOffset in range (-1,2):
+                #   If the spot is in bounds and there is a piece to jump over...
+                if self.isInBounds(x+rowOffset, y+columnOffset) and \
+                                self.listBoard[(x+rowOffset) * self.dimen + (y+columnOffset)][1] is not "empty":
+                    newX = x + (rowOffset*2)
+                    newY = y + (columnOffset*2)
+                    #   If the spot after the jump is in bounds and isn't already occupied...
+                    if self.isInBounds(newX, newY) and self.listBoard[newX * self.dimen + newY][1] is "empty":
+                        legalMoves.append([newX, newY])
+                        self.findJumps(visited, legalMoves, newX, newY)
+
+
+    def isInBounds(self, x,y):
+        if x<0 or x>= self.dimen or y<0 or y >=self.dimen:
+            return False
+        else:
+            return True
 board = Board(10)
